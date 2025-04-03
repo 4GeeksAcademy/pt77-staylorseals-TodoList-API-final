4262b22523f6b9b8204b12cf2834d322c8be778e
Sierrena Taylor-Seals <sierrena.taylor@outlook.com>
2025-04-03 17:42:03 +0000

    todo list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	
+	const [todos, setTodos] = useState(["check email", "do laundry"]);	
+	const [inputValue, setInputValue] = useState("");
+	
+	const addTodo = () => {
+		if (inputValue.trim() !== '') {
+			setTodos([...todos, inputValue]);
+			setInputValue("");
+		}
+	};
+	
+	const handleInputChange = (event) => {
+			setInputValue(event.target.value);
+	};
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const handleKeyDown = (event) => {
+		if (event.key === 'Enter') {
+			addTodo();
+		}
+	};
+
+	const deleteTodo = (indexToDelete) => {
+		setTodos(todos.filter((_, index) => index !== indexToDelete));
+	};
+
+	return (
+			<div className="home-container">
+				<h1>ToDo List</h1>
+				<div>
+					<input 
+						type="text" 
+						placeholder="What needs to be done?" 
+						value={inputValue} 
+						onChange={handleInputChange}
+						onKeyDown={handleKeyDown}
+					/>
+					<button type="button" onClick={addTodo}>
+						Add
+					</button>
+				</div>
+				<ul>
+					{todos.length === 0 ? (
+						<li className="empty-list">No tasks, add a task</li>
+					) : (
+						todos.map((todo, index) => (
+							<li key={index} className="todo-item">
+								{todo}
+								<button
+									className="delete-button"
+									onClick={() => deleteTodo(index)}
+								>
+									Delete
+								</button>
+							</li>
+					)))
+					}
+				</ul>
+			</div>
+		
 	);
 };
 
